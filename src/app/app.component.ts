bd2fbd22b94e4a441c7f0f82645123b8dc439876
Thales Vinicius Dias <thales.dias@unidas.com.br>
2020-07-09 08:02:25 -0300

    Upgrate version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//import { MatIconRegistry } from '@angular/material';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { icons } from './app.icons'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'myProject';
+
+  constructor(
+    //private matIconRegistry: MatIconRegistry,
+    private router: Router,
+    public _translate: TranslateService,
+  ) {
+    //this.registerIcons();
+  }
+
+  // registerIcons(): void {
+  //   icons.forEach(element => {
+  //     this.matIconRegistry.addSvgIcon(
+  //       element.iconName,
+  //       this.domSanitizer.bypassSecurityTrustResourceUrl(element.url)
+  //     );
+  //   });
+  // }
 }
